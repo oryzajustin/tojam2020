4db78f51608df6fefa8a448b3bd2ede5543334a2
Justin <jkoh8@uwo.ca>
2020-05-09 13:43:47 -0400

    player rotation system

---
+++ Assets/Scripts/System/PlayerRotationSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class PlayerRotationSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Rotation rotation, in Translation position, in MoveData move_data) => 
+        {
+            if (!move_data.direction.Equals(float3.zero)) // if we are moving
+            {
+                quaternion target_rotation = quaternion.LookRotation(move_data.direction, math.up()); // target rotation
+                rotation.Value = math.slerp(rotation.Value, target_rotation, move_data.turnSpeed); // manipulate the game object transform's rotation value using the turn speed
+            }
+        }).Schedule(); // worker thread
+    }
+}
