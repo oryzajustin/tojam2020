d15d891e3a7760ec485d883fa57c439a86d0585f
Justin <jkoh8@uwo.ca>
2020-05-10 17:42:03 -0400

    main menu

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    [SerializeField] Transform pivot;
+    [SerializeField] float orbit_speed;
+    void Update()
+    {
+        transform.RotateAround(pivot.position, Vector3.up, orbit_speed * Time.deltaTime);
+    }
+}
