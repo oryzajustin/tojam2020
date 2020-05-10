3d3db7fc605f04f7872ece78708a518c9d767a3a
YuanC <jerrych.97@gmail.com>
2020-05-09 20:09:11 -0400

    nuke da ecs

--- Assets/Scripts/Data/PlayerInputData.cs
+++
@@ -1,8 +0,0 @@
-﻿using Unity.Entities;
-using UnityEngine;
-
-[GenerateAuthoringComponent]
-public struct PlayerInputData : IComponentData
-{
-    public KeyCode up_key, right_key, left_key, down_key; // vars for holding directional key codes i.e. (W, A, S, D)
-}
--- Assets/Scripts/Data/TargetData.cs
+++
@@ -1,7 +0,0 @@
-﻿using Unity.Entities;
-
-[GenerateAuthoringComponent]
-public class TargetData : IComponentData
-{
-    public Entity targetEntity;
-}
