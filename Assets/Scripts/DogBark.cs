3055cfc764be79c265eb9e58459458156c2e12b2
Justin <jkoh8@uwo.ca>
2020-05-10 15:30:18 -0400

    dog barking

--- Assets/Scripts/DogBark.cs
+++ Assets/Scripts/DogBark.cs
@@ -6,9 +6,13 @@ public class DogBark : MonoBehaviour
 {
     [SerializeField] SheepManager sheep_manager;
     private Animator animator;
+
+    [SerializeField] List<AudioClip> barking_sounds;
+    private AudioSource audio_source;
     // Start is called before the first frame update
     void Start()
     {
+        audio_source = this.GetComponent<AudioSource>();
         animator = this.GetComponent<Animator>();
     }
 
@@ -19,6 +23,14 @@ public class DogBark : MonoBehaviour
         {
             animator.SetTrigger("bark");
             sheep_manager.DisperseSheepFromPosition(this.transform.position);
+            Bark();
         }
+    }
+
+    private void Bark()
+    {
+        int random_bark = Random.Range(0, barking_sounds.Count);
+        audio_source.clip = barking_sounds[random_bark];
+        audio_source.Play();
     }
 }
