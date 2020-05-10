9b795467d78518b86ea505405c7a93a0180a9e12
YuanC <jerrych.97@gmail.com>
2020-05-10 13:22:45 -0400

    sheeps

---
+++ Assets/Scripts/Sheep.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    public float Speed;
+    private Vector3 runDir;
+    private float runSpeed;
+    private bool dispersing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        runDir = new Vector3(0, 0, 0);
+        dispersing = false;
+    }
+
+    void Update()
+    {
+        if (dispersing)
+        {
+            
+        }
+        else
+        {
+
+        }
+
+    }
+
+    public void DisperseInDirection(Vector3 direction, float scalar)
+    {
+        runDir = direction.normalized;
+        runSpeed = Speed * scalar;
+    }
+}
---
+++ Assets/Scripts/SheepManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepManager : MonoBehaviour
+{
+    public List<Sheep> SheepList;
+    public float DisperseDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SheepList = new List<Sheep>(GetComponentsInChildren<Sheep>());
+    }
+
+    public void DisperseSheepFromPosition()
+    {
+
+    }
+}
