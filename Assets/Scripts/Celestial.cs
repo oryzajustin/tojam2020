c6685a1fbe423545397863961c93756a6b691606
Justin <jkoh8@uwo.ca>
2020-05-10 17:59:02 -0400

    day n night WAH WAH

---
+++ Assets/Scripts/Celestial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Celestial : MonoBehaviour
+{
+    [SerializeField] float orbit_speed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(new Vector3(0, 50, 0), Vector3.right, orbit_speed * Time.deltaTime);
+    }
+}
